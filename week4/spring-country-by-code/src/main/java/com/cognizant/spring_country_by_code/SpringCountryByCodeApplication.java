7a06ae2bf045d550c355298bb12d630f50002aeb
Sharika <sharikaciddam@gmail.com>
2025-07-12 18:29:56 +0530

    Added /countries/{code} REST API to return Country from XML

---
+++ week4/spring-country-by-code/src/main/java/com/cognizant/spring_country_by_code/SpringCountryByCodeApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_country_by_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCountryByCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCountryByCodeApplication.class, args);
+	}
+
+}
