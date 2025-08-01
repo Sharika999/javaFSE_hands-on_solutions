a0df30ffa684ebe97a76b29c24ba4b19bcb5192b
Sharika <sharikaciddam@gmail.com>
2025-08-01 23:26:23 +0530

    Add 13. ReactJS-HOL: BloggerApp with conditional rendering and multiple components

---
+++ week7/13. ReactJS-HOL/src/components/BlogDetails.js
@@ -0,0 +1,12 @@
+ 
+import React from 'react';
+
+const BlogDetails = () => (
+  <div>
+    <h2>Blog Details</h2>
+    <p>Title: Learning React in 2025</p>
+    <p>Author: Sharika</p>
+  </div>
+);
+
+export default BlogDetails;
---
+++ week7/13. ReactJS-HOL/src/components/BookDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BookDetails = () => (
+  <div>
+    <h2>Book Details</h2>
+    <p>Title: React Essentials</p>
+    <p>Author: John Doe</p>
+  </div>
+);
+
+export default BookDetails;
+ 
